148ccde8942fd390a1e1caa979f7a8e4f420301c
msoler18 <msoler735@gmail.com>
2018-04-20 14:41:44 -0500

    Create sessions controller and update Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :users
+  
+  get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#create'
+
   resources :posts
   resources :users, only: [:new, :create]	  
 end
