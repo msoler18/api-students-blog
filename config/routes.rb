9087afe30a6bf62af30b4c7fd573c7a6833d94a6
msoler18 <msoler735@gmail.com>
2018-04-20 15:15:17 -0500

    Function Auth User

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
   
   get 'login', to: 'sessions#new'
-  get 'login', to: 'sessions#create'
+  post 'login', to: 'sessions#create'
 
   resources :posts
   resources :users, only: [:new, :create]	  
