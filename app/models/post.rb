231208b414af68e859272b2ab30c234206a8e7ae
msoler18 <msoler735@gmail.com>
2018-04-20 19:54:38 -0500

    Add details for Show

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :content, length: {minimum: 250}
+  validates_associated :user
 end
